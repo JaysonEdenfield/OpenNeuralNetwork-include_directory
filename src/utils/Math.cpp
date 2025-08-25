c52e24bcedd557b1e77de0ba4b6281cd44eb3cfd
JaysonEdenfield <137231019+JaysonEdenfield@users.noreply.github.com>
2025-08-24 23:27:08 -0400

    Update Math.cpp
    update header path for new include directory

--- src/utils/Math.cpp
+++ src/utils/Math.cpp
@@ -1,4 +1,4 @@
-#include "../../include/utils/Math.hpp"
+#include "utils/Math.hpp"
 
 void utils::Math::multiplyMatrix(Matrix *a, Matrix *b, Matrix *c) {
     for (int i = 0; i < a->getNumRows(); i++) {
